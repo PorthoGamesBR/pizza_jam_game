5ff326d8665da1e6932b075588a3a8e4adf23fe6
GuiHSM <Guihenrisampaio@gmail.com>
2024-02-19 20:29:42 -0300

    feat: tile map exporting vars

--- levels/TileMap.gd
+++ levels/TileMap.gd
@@ -1,8 +1,8 @@
 extends TileMap
-var chance = [[325,25,25,3,3],[1, 25, 25, 3,12]]
+@export var mapHeight : int = 32
+@export var mapWidth : int = 32
+var chance : Array[Array] = [[325,25,25,3,3],[1, 25, 25, 3,12]]
 var rng = RandomNumberGenerator.new()
-var mapHeight = 32
-var mapWidth = 32
 
 func _ready():
 	var total = 0
@@ -12,14 +12,14 @@ func _ready():
 	for x in range(mapHeight):
 		for y in range(mapWidth):
 			var random = rng.randf_range(0, total)
-			var final = getVector(random)
+			var final = getAtlasId(random)
 			set_cell(0,Vector2i(x-mapWidth/2,y-mapHeight/2),0,final)
 		
 
 func _process(delta):
 	pass
 
-func getVector(val):
+func getAtlasId(val):
 	var y = 0
 	var total=0
 	for array in chance:
