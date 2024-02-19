03794514b8100ab1121b5dde9f16034d05ec0ab8
GuiHSM <Guihenrisampaio@gmail.com>
2024-02-19 19:41:45 -0300

    tech: changed to use a array in procedural tile set

--- levels/TileMap.gd
+++ levels/TileMap.gd
@@ -1,35 +1,34 @@
 extends TileMap
-
+var chance = [[325,25,25,3,3],[1, 25, 25, 3,12]]
 var rng = RandomNumberGenerator.new()
 var mapHeight = 32
 var mapWidth = 32
 
 func _ready():
+	var total = 0
+	for x in chance:
+		for y in x:
+			total+=y
 	for x in range(mapHeight):
 		for y in range(mapWidth):
-			var random = rng.randf_range(0, 450)
-			var final = Vector2(0,0)
-			if random>325:
-				final = Vector2(1,0)
-			if random>350:
-				final = Vector2(2,0)
-			if random>375:
-				final = Vector2(2,1)
-			if random>400:
-				final = Vector2(1,1)
-			if random>424:
-				final = Vector2(0,1)
-			if random>425:
-				final = Vector2(3,0)
-			if random>432:
-				final = Vector2(4,0)
-			if random>435:
-				final = Vector2(3,1)
-			if random>438:
-				final = Vector2(4,1)
+			var random = rng.randf_range(0, total)
+			var final = getVector(random)
 			set_cell(0,Vector2i(x-mapWidth/2,y-mapHeight/2),0,final)
 		
 
 
 func _process(delta):
 	pass
+
+func getVector(val):
+	var y = 0
+	var total=0
+	for array in chance:
+		var x = 0
+		for value in array:
+			if(val<total+value):
+				return Vector2(x,y)
+			x+=1
+			total+=value
+		y+=1
+	return Vector2(0,0)
