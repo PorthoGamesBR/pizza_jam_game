57ce079a86b552f03fc36a03108f6cbaaa3f9051
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2024-02-22 20:45:26 -0300

    fix: Changed bullet physics to change dir

--- particles/bullet.gd
+++ particles/bullet.gd
@@ -1,9 +1,12 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed = 750
 
+func _ready():
+	velocity = Vector2(speed,0)
+	
 func _physics_process(delta):
-	position += transform.x * speed * delta
+	move_and_collide(velocity * delta)
 
 func _on_does_damage_contacted_solid():
 	queue_free()
