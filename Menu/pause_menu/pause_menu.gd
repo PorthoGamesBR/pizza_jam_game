5361116f5325eb1823e25d49fb543113b988f250
GuiHSM <Guihenrisampaio@gmail.com>
2024-02-23 20:03:19 -0300

    fix: fixed buttons in menus

--- Menu/pause_menu/pause_menu.gd
+++ Menu/pause_menu/pause_menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 func _ready():
 	hide()
-	game_manager.connect("toggle_game_paused", on_game_manager_toogle_game_paused)
+	game_manager.toggle_game_paused.connect(on_game_manager_toogle_game_paused)
 
 func on_game_manager_toogle_game_paused(is_paused:bool):
 	if is_paused:
@@ -12,11 +12,9 @@ func on_game_manager_toogle_game_paused(is_paused:bool):
 	else:
 		hide()
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://levels/game_level.tscn")
+func _on_exit_pressed():
+	game_manager.game_paused = !get_tree().paused
+	get_tree().change_scene_to_file("res://Menu/Base/base_menu.tscn")
 
-func _on_quit_pressed():
-	get_tree().quit()
-
-func _on_options_pressed():
-	get_tree().change_scene_to_file("res://Menu/Base/options_menu.tscn")
+func _on_resume_pressed():
+	game_manager.game_paused = !get_tree().paused
