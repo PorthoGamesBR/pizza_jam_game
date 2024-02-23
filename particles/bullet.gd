01bc9b0a1f4f28a9cdc3d92990dd6a04509ca680
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2024-02-22 21:07:13 -0300

    feat: Bullet follows mouse

--- particles/bullet.gd
+++ particles/bullet.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
 @export var speed = 750
+@export var direction : Vector2 = Vector2(1,1)
 
 func _ready():
-	velocity = Vector2(speed,0)
-	
+	velocity = direction * speed
+	direction = (get_global_mouse_position() - get_position_delta()).normalized()
+
 func _physics_process(delta):
-	move_and_collide(velocity * delta)
+	move_and_collide(velocity * delta * direction)
 
 func _on_does_damage_contacted_solid():
 	queue_free()
